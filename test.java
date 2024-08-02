084ca907b887b089226c093e4c7b5b08f75821f7
Alvaro RPL <keyza_faiq_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:53:49 +0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Keyza Alvaro Faiq dari kelas X RPL 7 yang berasal dari kota Sidoarjo");
+        String a = "1";
+    }
+}
+
+
