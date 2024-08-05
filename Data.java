23143d4513dbc2261095721847c7b5225fe0ca3f
Alvaro RPL <keyza_faiq_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:36:29 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,18 @@
+public class Data {
+    public static void main(String[] args) {
+        //tipe_data nama_variable = value/isi variable;
+        String nama = "Varo";
+        int usia = 15;
+        String alamat = "The Taman Dhika, Buduran, Sidoarjo";
+        double kelas = 10.7;
+
+    System.out.println("Nama saya " + nama);
+    System.out.println("Umur saya " + usia + " tahun");
+    System.out.println("Alamat rumah saya di perumahan " + alamat);
+    System.out.println("Saya kelas " + kelas);
+    
+
+    
+    }
+    
+}
