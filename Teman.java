137ab5dc177e2110a14af267e36322aa89d58288
Alvaro RPL <keyza_faiq_33rpl@student.smktelkom-mlg.sch.id>
2024-08-12 07:35:03 +0700

    Add files via upload

---
+++ Teman.java
@@ -0,0 +1,12 @@
+public class Teman {
+        
+        int absen;
+        String nama;
+        double ipk;
+        public Teman() {
+            absen = 0;
+            nama = "empty";
+            ipk = 0.0;
+        }
+    }
+    
